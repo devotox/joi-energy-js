d2b42c3723c42e0f1c5cfd50600b0260bd977ffd
Devonte <devo.tox.89@gmail.com>
2019-09-30 09:59:31 +0100

    working

--- src/controller/electricity-reading-controller.js
+++ src/controller/electricity-reading-controller.js
@@ -4,6 +4,9 @@ const express = require('express')
 const router = express.Router()
 const ElectricityReadingService = require('../service/electricity-reading-service')
 const InvalidJsonException = require('../service/invalid-json-exception')
+const PricePlanService = require('../service/price-plan-service')
+const AccountService = require('../service/account-service')
+
 
 router.post('/store', function(req, res) {
     let electricityReadingService = new ElectricityReadingService()
@@ -24,6 +27,20 @@ router.get('/read/:smartMeterId', function(req, res) {
     } else {
         res.status(200).send(electricityReadingService.retrieveReadingsFor(smartMeterId))
     }    
+})
+
+router.get('/read/:smartMeterId/usage', function(req, res) {
+   const smartMeterId = req.params.smartMeterId
+    const pricePlanService = new PricePlanService()
+    const accountService = new AccountService();
+    const pricePlan = accountService.getPricePlan(smartMeterId);
+    
+    if (!pricePlan) {
+        res.status(404).send(`No tariff found for the smart meter ${smartMeterId}`)
+    } else {
+        let response = pricePlanService.getUsageData(smartMeterId, pricePlan)
+        res.status(200).send(response)
+    }
 })
 
 module.exports = router
--- src/service/price-plan-service.js
+++ src/service/price-plan-service.js
@@ -16,25 +16,53 @@ class PricePlanService {
         if (readings.length < 1) return []
         let average = this.calculateAverageReading(readings)
         let timeElapsed = this.calculateTimeElapsed(readings)
-        let consumedEnergy = average/timeElapsed
 
+        // usage in kWh = average reading * usage time
+        let consumedEnergy = average * timeElapsed // should be multiplied
+        // console.log('Consumed Energy', consumedEnergy, ' | ', average * timeElapsed, ' | ', average)
+        // console.log(readings)
+
+        // cost = tariff unit price * usage
         let pricePlans = pricePlanRepository.get()
         return this.cheapestPlansFirst(pricePlans).map(pricePlan => {
             let cost = {}
-            cost[pricePlan.name] = consumedEnergy * pricePlan.unitRate
+            cost[pricePlan.name] = consumedEnergy * pricePlan.unitRate 
             return cost;
         })
+    }
+
+    getUsageData(smartMeterId, pricePlan) {
+        let readings = this.electricityReadingService.retrieveReadingsFor(smartMeterId)
+        if (!readings.length) return { cost: 0 }
+
+        let average = this.calculateAverageReading(readings)
+        let timeElapsed = this.calculateTimeElapsed(readings)
+
+        // usage in kWh = average reading * usage time
+        let consumedEnergy = average * timeElapsed // should be multiplied
+        console.log('Consumed Energy', consumedEnergy, ' | ', average * timeElapsed, ' | ', average)
+
+        let plan = pricePlanRepository.get().find(p => p.name === pricePlan)
+        let cost = plan.unitRate * consumedEnergy
+
+        return {
+            consumedEnergy,
+            pricePlan,
+            cost: cost.toFixed(2)
+        }
     }
 
     cheapestPlansFirst(pricePlans) {
         return pricePlans.sort((planA, planB) => planA.unitRate - planB.unitRate)
     }
 
+    // average reading in kW = (eR[1].reading + eR[2].reading) / 2 
     calculateAverageReading(readings) {
         let sum = readings.map(r=>r.reading).reduce((p,c) => p+c, 0)
-        return sum / readings.length                
+        return sum / readings.length          
     }
 
+    // usage time in hours = eR[2].time - eR[1].time
     calculateTimeElapsed(readings) {
         let min = Math.min.apply(null, readings.map(r=>r.time))
         let max = Math.max.apply(null, readings.map(r=>r.time))
